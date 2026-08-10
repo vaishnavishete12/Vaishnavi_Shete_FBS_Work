c3efc5981423c1b920b55d4594bcdda28b83c3a2
vaishnavishete12 <shetevaishnavi8@gmail.com>
2026-08-10 18:35:07 +0530

    Delete Assignment 3 directory

--- Assignment 3/assi3_q2.py
+++
@@ -1,8 +0,0 @@
-# Program to check whether an alphabet is vowel or consonant
-
-ch = input("Enter an alphabet: ")
-
-if ch in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):
-    print("Vowel")
-else:
-    print("Consonant")
