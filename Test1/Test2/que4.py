436df3abc84367a8802a80c7745b26875b1e6aaa
vaishnavishete12 <shetevaishnavi8@gmail.com>
2026-08-14 16:33:46 +0530

    Delete Test1/Test2/que4.py

--- Test1/Test2/que4.py
+++
@@ -1,9 +0,0 @@
-#Write a program to print pattern.
-
-for i in range(5):
-    for j in range(5):
-        if (i + j) % 2 == 0:
-            print("1", end="")
-        else:
-            print("0", end="")
-    print()
