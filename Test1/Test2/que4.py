53049c2322fd0cea431346c16733c50d6c2e96ee
vaishnavishete12 <shetevaishnavi8@gmail.com>
2026-08-14 16:30:38 +0530

    Add files via upload

---
+++ Test1/Test2/que4.py
@@ -0,0 +1,9 @@
+#Write a program to print pattern.
+
+for i in range(5):
+    for j in range(5):
+        if (i + j) % 2 == 0:
+            print("1", end="")
+        else:
+            print("0", end="")
+    print()
